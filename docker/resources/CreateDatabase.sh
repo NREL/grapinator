10ca3144eec08b606baa17d65dbbffd04b5cf82d
David Martin <david.martin@nrel.gov>
2025-05-28 11:11:39 -0600

    Updates for issue #11.  Close complete.

--- docker/resources/CreateDatabase.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd /opt/mariadb/test_db-master
-mysql -uroot -p"f00baZZ123" < employees.sql
-echo "Database employeesdb setup complete."
-cd sakila
-mysql -uroot -p"f00baZZ123" < sakila-mv-schema.sql
-mysql -uroot -p"f00baZZ123" < sakila-mv-data.sql
-echo "Database sakiladb setup complete."
